199806cff6fb7c1806cc5e11938af527c142223f
Arihant Nahar <arihant.nahar@protonshub.in>
2024-01-16 12:24:58 +0530

    refactor update quality code

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -7,46 +7,28 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+        item.quality = item.quality - 1 if item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0 
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
+            item.quality = item.quality + 1 if item.sell_in < 11 and item.quality < 50            
+            item.quality = item.quality + 1 if item.sell_in < 6 and item.quality < 50
           end
         end
       end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
+
+      item.sell_in = item.sell_in - 1 if item.name != "Sulfuras, Hand of Ragnaros" 
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
+            item.quality = item.quality - 1 if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros" 
           else
             item.quality = item.quality - item.quality
           end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
+        elsif item.quality < 50 
+          item.quality = item.quality + 1 
         end
       end
     end
