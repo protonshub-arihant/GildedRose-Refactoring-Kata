4766c142156822ec795abd5cec9ecf5907eb19da
Arihant Nahar <arihant.nahar@protonshub.in>
2024-01-16 12:31:10 +0530

    added code for conjured functionality

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -7,7 +7,13 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        item.quality = item.quality - 1 if item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0 
+        if item.name != "Sulfuras, Hand of Ragnaros" and item.quality > 0
+          if item.name != "Conjured Mana Cake"
+              item.quality = item.quality - 1
+          else
+              item.quality = item.quality - 2
+          end
+        end 
       else
         if item.quality < 50
           item.quality = item.quality + 1
@@ -23,7 +29,8 @@ class GildedRose
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            item.quality = item.quality - 1 if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros" 
+            item.quality = item.quality - 1 if item.quality > 0 and item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1 if item.quality > 0 and item.name == "Conjured Mana Cake"
           else
             item.quality = item.quality - item.quality
           end
